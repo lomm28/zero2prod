89a5426423c04fcbb276c5dac79a351e7960faee
Dmytro D <brandnewgarage@gmail.com>
2023-08-17 20:59:14 +0300

    fix: tcp listener added

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,15 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-
-    format!("Hello {}!", &name)
-}
+use std::net::TcpListener;
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8002")?
-    .run()
-    .await
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
+
+    Ok(server)
 }
